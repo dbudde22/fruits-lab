a0c1f912b999a4a4d76890bcf3b6d477b76c2891
Dennis Budde <dbudde@texasmonthly.com>
2023-07-06 09:21:14 -0400

    fruits-lab tasks

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// CODE 
+app.use(cors())
+app.use(logger('dev'))
+// the following middleware comes out of the box with express...
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+// ROUTES 
+const fruits = ['apple','banana','orange','kumquat','tomato'];
+const veggies = ['carrot','broccoli','spinach','radish','peas'];
+
+app.get('/hello', (req, res) => {
+    console.log('hello world!')
+    res.send('hello world!')
+  
+  })
+
+app.get('/greet/:name', (req, res) => {
+    res.send(`Why hello there, ${req.params.name}`)
+})
+
+app.get('/five', (req, res) => {
+    const nums = [1,2,3,4,5];
+    res.send(nums);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send(fruits);
+})
+
+app.get('/fruits/:name', (req,res, next) => {
+    const fruit = fruits[req.params.name];
+    for (i=0; i<fruits.length; i++) {
+        if (fruits[i] == req.params.name) {
+            res.send(req.params.name);
+        }
+    }
+    next();
+})
+
+app.get('/fruits/sort', (req, res) => {
+    const sortedFruits = fruits.sort(); 
+    res.send(sortedFruits);
+})
+  
+app.get('/veggies', (req, res) => {
+    res.send(veggies);
+})
+
+app.get('/veggies/:name', (req,res) => {
+    const veggie = veggies[req.params.name];
+    res.send(veggie);
+})
+
+
+// catch-all route
+app.get('*', (req, res) => {
+    res.send('404 Not Cheese')
+  })
+  
+
+// BIND TO PORT 
+app.listen(PORT, () => console.log(`Serving up delicious fruits on port ${PORT} 🍒`))
